c9ba6fb046c7885d983eca1c4a34f792b06593ed
maury <maury.martinez@tutanota.com>
2025-05-22 13:59:44 +0200

    Setup project

---
+++ src/test/java/com/development/book/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
